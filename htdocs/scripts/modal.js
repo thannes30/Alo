cb4203e1c8b13588baa564048796ee16423f812b
thannes30 <thannes30@yahoo.com>
2014-10-14 22:52:06 -0400

    added footer gcc logo and link

--- htdocs/scripts/modal.js
+++ htdocs/scripts/modal.js
@@ -1,7 +1,13 @@
-$(window).load(function(){
-  $('#myModal').modal('show');
+// $(window).load(function(){
+//   $('#myModal').modal('show');
+//   $(this).parent().css('z-index', 500);
+//   window.setTimeout(hideModal, 1000);
+// });
 
-  setTimeout(function(){
-    $('#myModal').modal('hide');
-  }, 1000);
-});
+// function showModal(){
+//   $('#myModal').modal('show');
+// }
+
+// function hideModal(){
+//   $('#myModal').modal('hide');
+// }
