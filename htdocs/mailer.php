0adef21b7ea18e45df05944843a108d5056a70b0
thannes30 <thannes30@yahoo.com>
2014-11-17 17:44:54 -0500

    final product

--- htdocs/mailer.php
+++ htdocs/mailer.php
@@ -5,7 +5,7 @@ $myemail = "info@alocinema.com";
 /* Check all form inputs using check_input function */
 $name = check_input($_POST['inputName'], "Your Name");
 $email = check_input($_POST['inputEmail'], "Your E-mail Address");
-$subject = check_input($_POST['inputSubject'], "Message Subject");
+$email_subject = check_input($_POST['inputSubject'], "Message Subject");
 $message = check_input($_POST['inputMessage'], "Your Message");
 
 /* If e-mail is not valid show error message */
@@ -23,7 +23,7 @@ Someone has sent you a message using your contact form:
 
 Name: $name
 Email: $email
-Subject: $subject
+Subject: $email_subject
 
 Message:
 $message
@@ -34,7 +34,7 @@ $message
 mail($myemail, $subject, $message);
 
 /* Redirect visitor to the thank you page */
-header('Location: http://alocinema.com');
+header('Location: http://alocinema.com/');
 exit();
 
 /* Functions we used */
